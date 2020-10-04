1dd9e372265e748757eaf799a537a01a761090f5
Larry McConville <larry.w.mcconville@gmail.com>
2020-10-03 19:27:02 -0500

    replaced getElementById(timer) with

--- scripts/script.js
+++ scripts/script.js
@@ -1,146 +1,144 @@
-var timerElement = document.getElementById("timer");
+// var timerElement = document.getElementById("timer");
 var startButton = document.getElementById("start");
-// var submitButton = document.getElementById("submit");
 var questionElement = document.getElementById("question");
 var ulContainer = document.getElementById("ul-container");
 var choiceListUl = document.getElementById("choice-list");
 
 var playerScore = 0;
-var secondsLeft = 120;
+var secondsLeft = 30;
 var question = 0;
-var answer = '';
+var answer = "";
 
 var questionArray = [
-    {
-        question: "What is 10/2?",
-        choices: ['3', '5', '115'],
-        answer: '5'
-    },
-    {
-        question: "What is 30/3?",
-        choices: ['6', '7', '10'],
-        answer: '10'
-    },
-    {
-        question: "What is 0*7?",
-        choices: ['0', '1', '7', '-7'],
-        answer: '0'
-    },
-    {
-        question: "Is 0 equal to null?",
-        choices: ['true', 'false'],
-        answer: 'false'
-    },
-    {
-        question: "Which number doesn't belong?",
-        choices: ['1', '2', '4', '8', '16', '32', '42', '64', '128', '256'],
-        answer: '42'
-    }
+  {
+    question: "What is 10/2?",
+    choices: ["3", "5", "115"],
+    answer: "5",
+  },
+  {
+    question: "What is 30/3?",
+    choices: ["6", "7", "10"],
+    answer: "10",
+  },
+  {
+    question: "What is 0*7?",
+    choices: ["0", "1", "7", "-7"],
+    answer: "0",
+  },
+  {
+    question: "Is 0 equal to null?",
+    choices: ["true", "false"],
+    answer: "false",
+  },
+  {
+    question: "Which number doesn't belong?",
+    choices: ["1", "2", "4", "8", "16", "32", "42", "64", "128", "256"],
+    answer: "42",
+  },
 ];
 
 var highScoreArray = [];
 
 function setTimer() {
-    var timerInterval = setInterval(function () {
-        secondsLeft--;
-        timerElement.textContent = "Timer: " + secondsLeft;
+  var timerInterval = setInterval(function () {
+    secondsLeft--;
+    // timerElement.textContent = "Timer: " + secondsLeft;
+    $("#timer").text(secondsLeft);
 
-        if (secondsLeft === 0 || question === questionArray.length) {
-            clearInterval(timerInterval);
-            quizOver();
-        }
-
-    }, 1000);
+    if (secondsLeft === 0 || question === questionArray.length) {
+      clearInterval(timerInterval);
+      quizOver();
+    }
+  }, 1000);
 }
 
 function quizOver() {
-    timerElement.textContent = "Quiz Over";
-    // Add quizOver housekeeping.
+  timerElement.textContent = "Quiz Over";
+  // Add quizOver housekeeping.
 }
 
 function presentQuestion() {
-    if (question < questionArray.length) {
-        questionElement.textContent = questionArray[question].question;
-        answer = questionArray[question].answer;
-        presentchoices();
-    } else {
-        quizOver();
-    }
+  if (question < questionArray.length) {
+    questionElement.textContent = questionArray[question].question;
+    answer = questionArray[question].answer;
+    presentchoices();
+  } else {
+    quizOver();
+  }
 }
 
 function presentchoices() {
-    //build a new <ul> for each set of new choices.
-    appendUlElement();
+  //build a new <ul> for each set of new choices.
+  appendUlElement();
 
-    //loop through choices and build a dynamic list of <li> for n choices.
-    for (var i = 0; i < questionArray[question].choices.length; i++) {
-        var choices = questionArray[question].choices[i];
-        // console.log(choices);
+  //loop through choices and build a dynamic list of <li> for n choices.
+  for (var i = 0; i < questionArray[question].choices.length; i++) {
+    var choices = questionArray[question].choices[i];
+    // console.log(choices);
 
-        var li = document.createElement("li");
-        li.textContent = choices;
-        li.setAttribute("data-index", i);
+    var li = document.createElement("li");
+    li.textContent = choices;
+    li.setAttribute("data-index", i);
 
-        choiceListUl.appendChild(li);
-    }
-};
+    choiceListUl.appendChild(li);
+  }
+}
 
 function appendUlElement() {
-    //build a new <ul> for each new question.
-    choiceListUl = document.createElement("ul");
-    choiceListUl.setAttribute("id", "choice-list");
-    document.getElementById("ul-container").appendChild(choiceListUl);
-};
+  //build a new <ul> for each new question.
+  choiceListUl = document.createElement("ul");
+  choiceListUl.setAttribute("id", "choice-list");
+  document.getElementById("ul-container").appendChild(choiceListUl);
+}
 
 function removeUlElement() {
-    //remove <ul> containing previous choices
-    choiceListUl.remove();
-};
+  //remove <ul> containing previous choices
+  choiceListUl.remove();
+}
 
 function checkAnswer(event) {
-    if (event === answer) {
-        console.log("Correct!!!");
-        playerScore++;
-    } else {
-        console.log("Wrong Answer!!!");
-        secondsLeft -= 10;
-    }
+  if (event === answer) {
+    console.log("Correct!!!");
+    playerScore++;
+  } else {
+    console.log("Wrong Answer!!!");
+    secondsLeft -= 10;
+  }
 
-    console.log('checkAnswer function, correct answer is  ' + answer);
-    console.log(event);
+  console.log("checkAnswer function, correct answer is  " + answer);
+  console.log(event);
 
-    question++;
+  question++;
 }
 
 function appendRadioControl(item, choice) {
-    var labelEl = document.createElement("label");
-    labelEl.setAttribute("for", item);
-    labelEl.setAttribute("class", "labelClass");
-    document.querySelector(".radio").appendChild(labelEl);
-    labelEl.textContent = item + ' : ' + choice;
+  var labelEl = document.createElement("label");
+  labelEl.setAttribute("for", item);
+  labelEl.setAttribute("class", "labelClass");
+  document.querySelector(".radio").appendChild(labelEl);
+  labelEl.textContent = item + " : " + choice;
 
-    var inputEl = document.createElement("input");
-    inputEl.setAttribute("type", "radio");
-    inputEl.setAttribute("name", "choice");
-    inputEl.setAttribute("class", "inputClass");
-    inputEl.setAttribute("id", item);
-    inputEl.setAttribute("value", item + ' : ' + choice);
-    document.querySelector(".radio").appendChild(inputEl);
-
+  var inputEl = document.createElement("input");
+  inputEl.setAttribute("type", "radio");
+  inputEl.setAttribute("name", "choice");
+  inputEl.setAttribute("class", "inputClass");
+  inputEl.setAttribute("id", item);
+  inputEl.setAttribute("value", item + " : " + choice);
+  document.querySelector(".radio").appendChild(inputEl);
 }
 
 startButton.addEventListener("click", function () {
-    setTimer();
-    presentQuestion();
-    startButton.style.display = "none";
+  setTimer();
+  presentQuestion();
+  startButton.style.display = "none";
 });
 
 //When an element inside of the ulContainer is clicked.
 ulContainer.addEventListener("click", function (event) {
-    var element = event.target;
-    // console.log(element);
-    // console.log(element.textContent);
-    checkAnswer(element.textContent);
-    removeUlElement();
-    presentQuestion();
-})
+  var element = event.target;
+  // console.log(element);
+  // console.log(element.textContent);
+  checkAnswer(element.textContent);
+  removeUlElement();
+  presentQuestion();
+});
